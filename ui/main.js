43c3c30aff3e2f91c117d8b104446bc661c9d697
sarvanan16 <sarvanan40@gmail.com>
2016-09-29 18:44:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,10 @@
 console.log('Loaded!');
+var img=document.getElementById('madi');
+function moveRight()
+{
+    marginleft=marginleft+10;
+    img.style.marginleft=marginleft+'px';
+}
+img.onclick=function(){
+    var interval=setinterval(moveRight,10);
+}
