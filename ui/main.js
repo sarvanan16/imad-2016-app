0c642dd0347c564e823c5979e26c68c0f0be28b2
sarvanan16 <sarvanan40@gmail.com>
2016-10-01 09:06:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,10 +4,11 @@ var marginLeft=0;
 function moveRight()
 {
     marginLeft=marginLeft+10;
+    document.body.style.backgroundColor =marginLeft;
     img.style.marginLeft=marginLeft+'px';
     
 }
 img.onclick=function(){
-    document.body.style.backgroundColor = "red";
+    
     var interval=setInterval(moveRight,1);
 };
