ca3d25385ed746c883c6c0fcbe249ab61814845b
sarvanan16 <sarvanan40@gmail.com>
2016-11-19 10:43:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'welcome.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'about.html'));
 });
 
 app.get('/addc', function (req, res) {
