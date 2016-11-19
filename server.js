939765d5b022347234770b1948cdb910e4fb26d3
sarvanan16 <sarvanan40@gmail.com>
2016-11-19 23:47:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -199,7 +199,7 @@ app.get('/act-register',function(req,res){
 app.get('/act-login',function(req,res){
     var name='nan';
     var roll='46';
-    pool.query('SELECT name FROM "test" WHERE id =' + name,function(err,result){
+    pool.query('SELECT name FROM "test" WHERE name =' + name,function(err,result){
         if(err)
         {
             res.send("wrong");
