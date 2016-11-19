dafad123fb28d1535945ed57eda3ccbf5f37c521
sarvanan16 <sarvanan40@gmail.com>
2016-11-19 22:07:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -180,7 +180,7 @@ app.get('/ui/voting.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'voting.png'));
 });
 
-/*app.get('/db', function (req, res) {
+app.get('/db', function (req, res) {
     
         pool.query('select * from test',function(err,result)
          {
@@ -197,7 +197,7 @@ app.get('/ui/voting.png', function (req, res) {
     
   
 });
-*/
+
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
