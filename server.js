9b2089605e915d46f6a8c7e726ff17322dd66a12
sarvanan16 <sarvanan40@gmail.com>
2016-11-20 10:01:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -178,8 +178,8 @@ app.get('/ui/voting.png', function (req, res) {
 });
 
 app.get('/act-register',function(req,res){
-    var name='nan';
-    var roll='46';
+    var name=prompt("Enter name");
+    var roll=prompt("Enter roll.no");
     pool.query('INSERT INTO "test" (name,roll) values($1,$2)',[name,roll],function(err,result){
         if(err)
         {
