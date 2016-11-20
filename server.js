90983688df18bbfa2c0c303fbd737e61a83768fa
sarvanan16 <sarvanan40@gmail.com>
2016-11-20 08:41:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -195,6 +195,37 @@ app.get('/act-register',function(req,res){
 
 
 
+app.get('/cor_register',function(req,res){
+    var username=document.getElementById('username').value;
+    var password=document.getElementById('inputpassword').value;
+    pool.query('SELECT username FROM "user" WHERE username = $1',[username],function(err,result){
+        if(err)
+        {
+            res.send("some error in 1st");
+        }
+        else
+        {
+            if(name==result.rows[0].username)
+            {
+                res.sendFile(path.join(__dirname, 'ui', 'register.html')); 
+            }
+            else
+            {
+                pool.query('INSERT INTO "user" (username,password) values($1,$2)',[username,password],function(err,result){
+                if(err)
+                {
+                    res.send("some error in 2nd");
+                   }
+               else
+               {
+                   res.sendFile(path.join(__dirname, 'ui', 'login.html'));
+                  }
+                });
+            }
+        }
+    
+    });
+});
 
 app.get('/cor_login',function(req,res){
     var username=document.getElementById('username').value;
