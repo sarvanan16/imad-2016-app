6a6d2308a2c568abcf3ba15c1f5b19978892d16e
sarvanan16 <sarvanan40@gmail.com>
2016-11-20 13:12:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,23 @@
 var submit=document.getElementById('submit_btn').value;
 function sub()
-{
+{   
+    var request = new XMLHttpRequest();
+        
+        // Capture the response and store it in a variable
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              // Take some action
+              if (request.status === 200) {
+                  submit.value = 'Sucess!';
+              } else if (request.status === 403) {
+                  submit.value = 'Invalid credentials. Try again?';
+              } else if (request.status === 500) {
+                  alert('Something went wrong on the server');
+                  submit.value = 'Login';
+              } else {
+                  alert('Something went wrong on the server');
+                  submit.value = 'Login';
+              }
     var username=document.getElementById('username').value;
     var password=document.getElementById('password').value;
     request.open('POST','http://sarvanan16.imad.hasura-app.io/login',true);
