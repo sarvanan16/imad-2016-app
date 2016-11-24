36f7e4106d93ddb7f2842c2775130c66daf077bf
sarvanan16 <sarvanan40@gmail.com>
2016-11-24 13:25:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -119,7 +119,7 @@ function loadArticles () {
                 for (var i=0; i< articleData.length; i++) {
                     content += `<li>
                     <a href="/articles/${articleData[i].title}">${articleData[i].heading}</a>
-                    (${articleData[i].date.split('T')[0]})</li><br>`;
+                    (${articleData[i].date.split('T')[0]})</li><br><br><br>`;
                 }
                 content += "</ul>"
                 articles.innerHTML = content;
